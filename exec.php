dcbabc6a6b40453093df3688469c1467f79409b9
Bryan <heliosbryan@gmail.com>
2016-02-15 23:07:06 +0200

    Helpers for executing shell scripts/programs

---
+++ exec.php
@@ -0,0 +1,10 @@
+function executeShell($command) {
+ $output = [];
+ $returnVar = null;
+ println("PHP is going to execute: " . $command);
+  exec($command, $output, $returnVar);
+  println("Output is:");
+  println($output);
+  println("Return value is: ");
+  println($returnVar);
+}
