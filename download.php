f3eac77b897e7c9e8c09e20446405516fe92ec21
Bryan <heliosbryan@gmail.com>
2016-02-16 01:58:42 +0200

    Fix colon->semicolon

--- download.php
+++ download.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 function download($url, $destinationFile, $mode = "wget") {
-switch($mode) {
-case "wget":
-shellExecute("wget $url -O  $destinationFile");
-break;
-default:
-throw new Exception("Unknown mode"):
-}
+    switch($mode) {
+        case "wget":
+            shellExecute("wget $url -O  $destinationFile");
+            break;
+        default:
+            throw new Exception("Unknown mode");
+    }
 }
