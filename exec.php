09c5c23737a0548cddc4db6e92b57a7f36e19711
Bryan <heliosbryan@gmail.com>
2016-02-16 00:30:02 +0200

    Multiple command support. Error code conversion to text value.

--- exec.php
+++ exec.php
@@ -1,12 +1,36 @@
 <?php 
 
+function execReturnValueToText($intValue) {
+    $text = null;
+
+    switch($intValue) {
+        case 0:
+            $text = "Success";
+            break;
+        case 127:
+            $text = "Command not found";
+            break;
+
+        default:
+            $text = "Unknown return code: " . $intValue;
+
+    }
+
+    return $text;
+}
+
+function executeShells($commands) {
+    $allCommands = implode("&&", $commands);
+    executeShell($allCommands);
+}
+
 function executeShell($command) {
- $output = [];
- $returnVar = null;
- println("PHP is going to execute: " . $command);
-  exec($command, $output, $returnVar);
-  println("Output is:");
-  println($output);
-  println("Return value is: ");
-  println($returnVar);
+    $output = [];
+    $returnVar = null;
+    println("PHP is going to execute: " . $command);
+    exec($command, $output, $returnVar);
+    println("Output is:");
+    println(implode("<br>", $output));
+    println("Return value is: ");
+    printDump(execReturnValueToText($returnVar));
 }
