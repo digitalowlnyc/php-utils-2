024f012fb87cc04a0f9f10a31e7d8317527e7616
Bryan <heliosbryan@gmail.com>
2016-02-16 01:44:48 +0200

    Log message so we know include worked

--- libraries.php
+++ libraries.php
@@ -1,7 +1,9 @@
 <?php
-
+echo "Including phputils...";
 include_once("print.php");
 include_once("errors.php");
 include_once("exec.php");
 include_once("download.php");
 include_once("filesystem.php");
+echo "Done loading phputils...";
+
