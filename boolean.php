ed43000d7931aff1a1a681d676409698db130f23
Bryan <heliosbryan@gmail.com>
2016-02-16 09:15:18 +0200

    Add some boolean to string helpers

---
+++ boolean.php
@@ -0,0 +1,12 @@
+<?php 
+
+function strYesNo($value) {
+    if($value === true)
+        return("yes");
+    return("no");
+}
+function strTrueFalse($value) {
+    if($value === true)
+        return("true");
+    return("false");
+}
