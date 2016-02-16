ea8d7b8abb30b6365b3d670c5fd14f5f15489376
Bryan <heliosbryan@gmail.com>
2016-02-16 02:20:02 +0200

    +func: tail error log

--- errors.php
+++ errors.php
@@ -6,3 +6,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 }
+
+function errorsTail() {
+  println("<pre>");
+  executeShell("tail error_log");
+  println("</pre>");
+}
