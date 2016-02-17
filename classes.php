e311635dd90289b2af9289dd1940d9a529d06002
Bryan <heliosbryan@gmail.com>
2016-02-18 01:59:51 +0200

    New loader for classes folder

---
+++ classes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bryan Mayor
+ * Date: 2/18/16
+ * Time: 1:45 AM
+ */
+require_once("includes.php");
+requireMultiple([
+    "./classes/FileReader.php",
+    "./classes/FileWriter.php",
+], true);
